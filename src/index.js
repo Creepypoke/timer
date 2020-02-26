2543dd4e73181dbea10029bfecb3e99671320926
Anton Yessaulenko <2114572+Creepypoke@users.noreply.github.com>
2020-02-26 11:13:14 +0300

    Add test service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import { Elm } from "./Main.elm";
-const swName = "/worker.js";
+const swName = "/sw.js";
 
 Elm.Main.init({
   node: document.querySelector("main#app")
 });
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register(swName)
+    .then(function(registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function(error) {
+      console.log("Service worker registration failed, error:", error);
+    });
+}
