1b5bb7d3f6438e7332b3ba024778073a846c81bb
Anton Yessaulenko <2114572+Creepypoke@users.noreply.github.com>
2020-02-26 12:22:59 +0300

    Add https local support

--- src/sw.js
+++ src/sw.js
@@ -13,7 +13,3 @@ workbox.routing.registerRoute(
   /http:\/\/cors-anywhere.herokuapp.com\/http:\/\/tycho.usno.navy.mil\/cgi-bin\/time.pl/i,
   workbox.strategies.networkFirst()
 );
-workbox.routing.registerRoute(
-  "https://cdn.rawgit.com/parcel-bundler/website/01a1f7dd/src/assets/parcel%403x.png",
-  workbox.strategies.cacheFirst()
-);
